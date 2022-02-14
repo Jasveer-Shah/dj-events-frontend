1a6fe81eed207ddc34d9f975d362ec38f4d61cdd
jasveer shah <talkjasveer@gmail.com>
2022-02-14 16:17:57 -0500

    add

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { API_URL } from '../config/index'
 import Link from 'next/link'
 
 export default function Home({events}) {
-  console.log(events)
+  console.log(events)        // client side cosole
   return (
     <Layout>
     <h1>Upcoming Events</h1>
@@ -29,9 +29,9 @@ export default function Home({events}) {
 export async function getStaticProps(){
 const res = await fetch(`${API_URL}/api/events`);
 const events = await res.json();
-  console.log(events)
+  console.log(events)  // it consoles on server side
 return {
-  props: {events:events.slice(0, 3)},
+  props: {events:events.slice(0, 3)},        // client side rendering
   revalidate: 1,
 }
 }
